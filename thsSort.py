dd11dcbc9a3c94b22498c43669a15a269bac192b
thatsOven <68780017+thatsOven@users.noreply.github.com>
2023-01-30 12:22:27 +0100

    update license

--- thsSort.py
+++ thsSort.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2021-2022 thatsOven
+Copyright (c) 2021-2023 thatsOven
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
