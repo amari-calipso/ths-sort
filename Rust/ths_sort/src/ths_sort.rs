c4bc37bf21b56db9aa207107160774c67917b1e4
thatsOven <68780017+thatsOven@users.noreply.github.com>
2023-05-05 09:00:04 +0200

    finish rust version, needs testing

--- Rust/ths_sort/src/ths_sort.rs
+++ Rust/ths_sort/src/ths_sort.rs
@@ -1,6 +1,9 @@
-use std::cmp::Ordering::{Less, Equal, Greater};
+use std::{cmp::Ordering::{Less, Equal, Greater}, ops::Div};
+
+use num_traits::{Num, NumOps};
 
 pub trait SortType: Ord + Copy {}
+pub trait NumType: SortType + Num {}
 
 const INCS: [usize; 5] = [48, 21, 7, 3, 1];
 const SWAPS: [usize; 120] = [
@@ -275,7 +278,7 @@ fn rotate<T: SortType>(array: &mut [T], mut a: usize, m: usize, mut b: usize) {
     }
 }
 
-fn binsearch<T: SortType>(array: &mut [T], mut a: usize, mut b: usize, val: T, l: bool) -> usize {
+fn binsearch<T: SortType>(array: &[T], mut a: usize, mut b: usize, val: T, l: bool) -> usize {
     while a < b {
         let m = a + ((b - a) / 2);
 
@@ -304,3 +307,243 @@ fn insert_sort<T: SortType>(array: &mut [T], a: usize, b: usize) {
     }
 }
 
+fn merge_up<T: SortType>(array: &mut [T], a: usize, m: usize, b: usize) {
+    let len = m - a;
+    let mut aux: Vec<T> = Vec::with_capacity(len);
+
+    for i in 0 .. len {
+        aux[i] = array[i + a];
+    }
+
+    let mut l = a;
+    let mut r = m;
+    let mut buf = 0;
+
+    while l < r && r < b {
+        if aux[buf].cmp(&array[r]).is_le() {
+            array[l] = aux[buf];
+            buf += 1;
+        } else {
+            array[l] = array[r];
+            r += 1;
+        }
+        l += 1;
+    }
+
+    while l < r {
+        array[l] = aux[buf];
+        l += 1;
+        buf += 1;
+    }
+}
+
+fn merge_down<T: SortType>(array: &mut [T], a: usize, m: usize, b: usize) {
+    let len = b - m;
+    let mut aux: Vec<T> = Vec::with_capacity(len);
+
+    for i in 0 .. len {
+        aux[i] = array[i + m];
+    }
+
+    let mut buf = len - 1;
+    let mut l = m - 1;
+    let mut r = b - 1;
+
+    while r > l && l >= a {
+        if aux[buf].cmp(&array[l]).is_ge() {
+            array[r] = aux[buf];
+            buf -= 1;
+        } else {
+            array[r] = array[l];
+            l -= 1;
+        }
+        r -= 1;
+    }
+
+    while r > l {
+        array[r] = aux[buf];
+        r -= 1;
+        buf -= 1;
+    }
+}
+
+#[inline]
+fn check_bounds<T: SortType>(array: &mut [T], a: usize, m: usize, b: usize) -> bool {
+    if array[m - 1].cmp(&array[m]).is_le() {
+        return true;
+    } else if array[a].cmp(&array[b - 1]) == Greater {
+        rotate(array, a, m, b);
+        return true;
+    }
+
+    return false;
+}
+
+fn merge_inplace<T: SortType>(array: &mut [T], a: usize, m: usize, b: usize) {
+    if m - a <= b - m {
+        let mut i = a;
+        let mut j = m;
+
+        while i < j && j < b {
+            if array[i].cmp(&array[j]) == Greater {
+                let k = binsearch(array, j, b, array[i], true);
+                rotate(array, i, j, k);
+                i += k - j;
+                j = k;
+            } else {
+                i += 1;
+            }
+        }
+    } else {
+        let mut i = m - 1;
+        let mut j = b - 1;
+
+        while j > i && i >= a {
+            if array[i].cmp(&array[j]) == Greater {
+                let k = binsearch(array, a, i, array[j], false);
+                rotate(array, k, i + 1, j + 1);
+                j -= i + 1 - k;
+                i = k - 1;
+            } else {
+                j -= 1;
+            }
+        }
+    }
+}
+
+fn merge<T: SortType>(array: &mut [T], mut a: usize, m: usize, mut b: usize) {
+    if check_bounds(array, a, m, b) {
+        return;
+    }
+
+    b = binsearch(array, m, b, array[m - 1], true);
+    a = binsearch(array, a, m - 1, array[m], false);
+
+    if b - m < m - a {
+        if b - m <= 16 {
+            merge_inplace(array, a, m, b);
+        } else {
+            merge_down(array, a, m, b);
+        }
+    } else {
+        if m - a <= 16 {
+            merge_inplace(array, a, m, b);
+        } else {
+            merge_up(array, a, m, b);
+        }
+    }
+}
+
+#[inline]
+fn get_reversed_runs<T: SortType>(array: &mut [T], a: usize, b: usize, limit: usize) -> bool{
+    let mut i = a;
+    while i < b - 1 {
+        if array[i].cmp(&array[i + 1]).is_le() {
+            break;
+        }
+    }
+
+    if i - a > limit {
+        reverse(array, a, i);
+    }
+
+    return i == b;
+}
+
+pub fn stable_sort<T: SortType>(array: &mut [T], a: usize, b: usize) {
+    if get_reversed_runs(array, a, b, 8) {
+        return
+    }
+
+    if b - a > 32 {
+        let m = a + ((b - a) / 2);
+
+        stable_sort(array, a, m);
+        stable_sort(array, m, b);
+
+        merge(array, a, m, b);
+    } else {
+        insert_sort(array, a, b);
+    }
+}
+
+
+
+#[inline]
+fn find_min_max<T: NumType>(array: &[T], a: usize, b: usize) -> (T, T) {
+    let mut min = array[a].clone();
+    let mut max = array[a].clone();
+
+    for i in a + 1 .. b {
+        if array[i] < min {
+            min = array[i];
+        } else if array[i] > max {
+            max = array[i];
+        }
+    }
+
+    return (min, max);
+}
+
+pub fn static_sort<T: NumType, GetFn>(array: &mut [T], a: usize, b: usize, get: GetFn) where GetFn: Fn(T) -> f32 {
+    let (min, max) = find_min_max(array, a, b);
+
+    let mut len = b - a + 1;
+    let mut cnt: Vec<usize> = vec![0; len];
+    let mut pts: Vec<usize> = vec![0; len];
+    len -= 1;
+
+    let mlt = len as f32 / (get(max) - get(min) + 1.0);
+
+    for i in a .. b {
+        cnt[((get(array[i]) - get(min)) * mlt) as usize] += 1;
+    }
+
+    pts[0] = a;
+    for i in 1 .. len {
+        pts[i] = cnt[i - 1] + pts[i - 1];
+    }
+
+    for i in 0 .. len {
+        while cnt[i] > 0 {
+            let orig     = pts[i];
+            let mut from = orig;
+            let mut val      = array[from];
+
+            loop {
+                let d = ((get(val) - get(min)) * mlt) as usize;
+                let to = pts[d];
+
+                pts[d] += 1;
+                cnt[d] -= 1;
+
+                let tmp = array[to];
+                array[to] = val;
+                val       = tmp;
+                from      = to;
+
+                if from == orig {
+                    break;
+                }
+            }
+        }
+    }
+
+    let mut s = a;
+    for i in 0 .. len {
+        let e = pts[i];
+
+        if e - s <= 1 {
+            s = pts[i];
+            continue;
+        }
+
+        if e - s > 16 {
+            max_heap_sort(array, s, e);
+        } else {
+            unchecked_insertion_sort(array, s, e);
+        }
+
+        s = pts[i];
+    }
+}
